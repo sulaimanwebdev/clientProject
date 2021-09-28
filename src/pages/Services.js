85fbcf0dcf728092656d701283d90ca8246549ce
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-09-28 13:14:09 +0500

    services half done

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,10 +1,227 @@
 import React from 'react'
-
+import '../css/services.css'
 const Services = () => {
     return (
-        <div className="services">
-            
+        <>
+        <div className="parent flex1P">
+            <div className="child flex1C">
+                 <div className="flex1Txt">
+                          <h1>Digital Experience Strategy & Managment</h1>
+                          <p>Next-level experiences demand Digital Experience Strategy & the Management solutions. </p>
+                              <a href="#">GET STARTED FOR FREE</a>
+                 </div>
+
+                 <div className="flex1Img"><img src="/images/servicesImages/flex1.svg" alt="" /></div>
+            </div>
         </div>
+
+
+
+
+
+
+
+
+
+        <div className="parent flex2P">
+            <div className="child flex2C">
+            <div className="flex2Img"><img src="/images/servicesImages/flex2.png" alt="" /></div>
+
+                 <div className="flex2Txt">
+                          <h1>Digital Experience Strategy & Management Strategy First</h1>
+                          <p>If you were building your dream home, you would not start by calling an interior designer; you would hire an architect. The same thing applies to building Digital Experience Solutions that drive meaningful value for your organization and your customers.  Customer Experience Strategy & Architecture come first.  
+<br /><br />
+Whether your team needs a little or a lot of help with your ongoing Digital Customer Experience (DCX) program, the Konabos team is here to help with
+everything from SEO, Content Writing, training, and more.</p>
+                              
+                 </div>
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+<div className="parent flex3P">
+    <div className="child flex3C">
+<div className="flex3Text">
+    <h1>Our Approach </h1>
+       <div className="iconsOflex">
+           <div className="iconIflex"><img src="/images/servicesImages/icon1.png"/> Business Goals Identification</div>
+           <div className="iconIflex"><img src="/images/servicesImages/icon2.png"/> Customer Persona & Journey Modeling</div>
+           <div className="iconIflex"><img src="/images/servicesImages/icon3.png"/> Identify Tactical Opportunities</div>
+           <div className="iconIflex"><img src="/images/servicesImages/icon4.png"/> Data Science Modelling</div>
+           <div className="iconIflex"><img src="/images/servicesImages/icon5.png"/> Omnichannel Content Strategy Development</div>
+           <div className="iconIflex"><img src="/images/servicesImages/icon6.png"/> UX4CX Design</div>
+           <div className="iconIflex"><img src="/images/servicesImages/icon7.png"/> DXP Design & Implementation</div>
+           
+       </div>
+ 
+
+       <p>Digital Experience success begins with Konabos’ Customer Experience Architecture approach, resulting in a fact-based, scientific method for informing the content strategy, business & customer data that ultimately drive the Customer Experience and User Experience (UX4CX) approach for your solution. </p>
+        <a href="#">START ARCHITECTING YOUR STRATEGY</a>
+
+</div>
+    </div>
+
+    <img src="/images/servicesImages/flex3Main.png" alt="" className="bgImage"/>
+
+</div>
+
+
+
+
+
+
+
+
+
+<div className="parent girdP">
+    <div className="child gridC">
+    <div className="circTitle">
+                <h1>Strategy Services</h1>
+            </div>
+
+
+            <div className="realGrid">
+
+<div className="rGrid1">
+    <img src="/images/servicesImages/gridIcon1.png" alt="" />
+    <h2>Search Engine Optimization (SEO)</h2>
+    <div className="line"></div>
+    <p>Are you looking for an SEO agency? We can help your business grow online through our search engine optimization services and strategies.</p>
+</div>
+
+
+
+
+
+
+<div className="rGrid1">
+    <img src="/images/servicesImages/gridIcon2.png" alt="" />
+    <h2>Content Writing Marketing</h2>
+    <div className="line"></div>
+    <p>The Konabos content marketing team specializes in SEO-friendly, bilingual blog content production to extend your website reach and engagement</p>
+</div>
+
+
+
+
+
+
+
+
+
+<div className="rGrid1">
+    <img src="/images/servicesImages/gridIcon3.png" alt="" />
+    <h2>Sitecore Personalization and Content Management Training</h2>
+    <div className="line lineB"></div>
+    <p>No matter what version of Sitecore you are on, there are things you can do RIGHT NOW to move the dial forward and begin to extract the ROI from Sitecore. </p>
+</div>
+
+
+
+
+
+
+
+
+<div className="rGrid1">
+    <img src="/images/servicesImages/gridIcon4.png" alt="" />
+    <h2>Social Media Marketing</h2>
+    <div className="line"></div>
+    <p>Our social media team can help highlight your services, engage with your audience, and reach new followers online through engagement strategies and multimedia content on various social channels. </p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div className="rGrid1">
+    <img src="/images/servicesImages/gridIcon5.png" alt="" />
+    <h2>Paid Advertising</h2>
+    <div className="line"></div>
+    <p>Boost your visibility, and attract more customers at an optimized acquisition cost.</p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div className="rGrid1">
+    <img src="/images/servicesImages/gridIcon6.png" alt="" />
+    <h2>User Experience and User Interface Design</h2>
+    <div className="line"></div>
+    <p>With expertise in UX/UI design and development for web, mobile, and social applications, we will work with you to create a UX strategy that will lead to an effective and efficient user experience. </p>
+</div>
+
+
+
+
+
+
+
+
+<div className="rGrid1">
+    <img src="/images/servicesImages/gridIcon7.png" alt="" />
+    <h2>Website Search</h2>
+    <div className="line"></div>
+    <p>Are you looking for insight and expertise to take the search on your website to the next level? Leave it to us.</p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+
+
+
+    </div>
+</div>
+
+
+
+
+
+
+</>
     )
 }
 
